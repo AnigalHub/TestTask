73ec912a9996266a4ac37b0e802c127ad2e939ac
AnigalHub <anigal1997@gmail.com>
2021-08-28 16:12:55 +0300

    страница с общими стилями, компонентами - сортировка, форма, товары'

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+gi<template>
+   <div>
+     <div class="background">
+       <div class="flex-container">
+         <h1>Добавление товара</h1>
+         <sorting/>
+       </div>
+       <div class="flex-container">
+         <form_submission/>
+         <products/>
+       </div>
+     </div>
+  </div>
+</template>
+
+<script>
+import Sorting from "~/components/sorting";
+import Form_submission from "~/components/form_submission";
+import Products from "~/components/products";
+export default {
+  components: {Products, Form_submission, Sorting},
+
+}
+</script>
+<style  lang="scss">
+  body{margin: 0 !important;}
+  .background{
+    margin-left: auto;
+    margin-right: auto;
+    width: 1440px;
+    position: relative;
+  }
+  .flex-container{display: flex;}
+  h1{
+    margin: 0 !important;
+    padding-top: 32px;
+    padding-left: 32px;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 35px;
+    color: #3F3F3F;
+    float: left;
+  }
+</style>
