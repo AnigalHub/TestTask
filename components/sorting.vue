7fff3e7e3ee3dd35f81f6cce5f07bca978ffb6f1
AnigalHub <anigal1997@gmail.com>
2021-09-22 00:17:39 +0300

    изменено все на dropdown в стилях, изменение названия с помощью функций по клику'

--- components/sorting.vue
+++ components/sorting.vue
@@ -1,44 +1,49 @@
 <template>
-  <div>
-<div id="sorting">По умолчанию
-      <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7.48532 1.24264L4.24268 4.48528L1.00003 1.24264" stroke="#B4B4B4"/>
-      </svg>
-    </div>
+  <div id="sorting">
     <div class="dropdown">
-
-      <div class="sort" @click="SortByPriceMinProducts()">По цене min</div>
-      <div class="sort" @click="SortByPriceMaxProducts()">По цене max</div>
-      <div class="sort" @click="SortByNameProducts()">По наименованию</div>
+      <span v-html="">
+       {{sort}}
+        <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M7.48532 1.24264L4.24268 4.48528L1.00003 1.24264" stroke="#B4B4B4"/>
+        </svg>
+      </span>
+      <div class="dropdown-content">
+        <div @click="SortByPriceMinProducts()">По цене min</div>
+        <div @click="SortByPriceMaxProducts()">По цене max</div>
+        <div @click="SortByNameProducts()">По наименованию</div>
+      </div>
     </div>
-   <!-- <select>
-      <option>По умолчанию</option>
-      <option>По цене min</option>
-      <option>По цене max</option>
-      <option>По наименованию</option>
-    </select> -->
   </div>
 </template>
 
 <script>
     export default {
       name: "sorting",
+      data() {
+        return {
+          sort:"По умолчанию",
+        }
+      },
+
       methods:{
         SortByNameProducts:function () {
           this.$store.dispatch('catalogStore/SortByName')
+          this.sort = "По наименованию"
         },
         SortByPriceMinProducts:function () {
           this.$store.dispatch('catalogStore/SortByPriceMin')
+          this.sort = "По цене min"
         },
         SortByPriceMaxProducts:function () {
           this.$store.dispatch('catalogStore/SortByPriceMax')
+          this.sort = "По цене max"
         },
       }
     }
 </script>
 
 <style scoped lang="scss">
-  #sorting,select,option{
+  .dropdown {
     float: left;
     width: 121.49px;
     height: 36px;
@@ -53,26 +58,34 @@
     font-family: 'Source Sans Pro', sans-serif;
     font-size: 12px;
     line-height: 15px;
-  }
-
-  .sort{
-    width: 121.49px;
-    background: #FFFEFB;
-    color: #B4B4B4;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    border-radius: 4px;
-    font-family: 'Source Sans Pro', sans-serif;
-    font-size: 10.5px;
-    line-height: 15px;
-    padding: 5px 16px;
-  }
-  .dropdown{
-    position: absolute;
-    margin-left: 1008px;
-    margin-top: 66px;
-    z-index: 1000;
-    div{
-      cursor: pointer;
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+    svg{
+     margin-left: 5px;
     }
+  }
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    top: 35px;
+    left: 0;
+    cursor: pointer;
+    z-index: 1;
+    div{
+      width: 121.49px;
+      background: #FFFEFB;
+      color: #B4B4B4;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+      border-radius: 4px;
+      font-family: 'Source Sans Pro', sans-serif;
+      font-size: 11px;
+      line-height: 15px;
+      padding: 5px 16px;
+    }
+  }
+  .dropdown:hover .dropdown-content {
+    display: block;
   }
 </style>
