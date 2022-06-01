e8e5fcea32056d3e4f335d67d7c2fa5b5d967a92
AnigalHub <anigal1997@gmail.com>
2022-06-02 00:10:31 +0300

    добавлен новый favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Страница продажи товаров',
+    title: 'Продажа товаров',
     htmlAttrs: {
       lang: 'en'
     },
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/icons.png' },
       {rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swa', media:'all', disabled:''},
 
     ]
